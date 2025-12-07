19eac7df04fbb75bd6a71616d6d876d6feb7af66
Sanjayazhagan <sanjayazhagan@gmail.com>
2025-12-07 12:33:52 +0530

    render

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -3,20 +3,25 @@ from pydantic_settings import BaseSettings
 from pydantic import field_validator
 
 class Setting(BaseSettings):
-    API_PREFIX:str="/api"
-    DEBUG:bool = False
+    API_PREFIX: str = "/api"
+    DEBUG: bool = False
 
-    DATABASE_URL:str
-    ALLOWED_ORIGINS:str=""
-
-    GOOGLE_API_KEY:str
-
+    DATABASE_URL: str
+    ALLOWED_ORIGINS: str = ""
+    GOOGLE_API_KEY: str
+    @field_validator("DATABASE_URL")
+    def fix_database_url(cls, v: str) -> str:
+        if v and v.startswith("postgres://"):
+            return v.replace("postgres://", "postgresql://", 1)
+        return v
     @field_validator("ALLOWED_ORIGINS")
-    def prase_allowed_origins(cls,v:str) -> List[str]:
+    def parse_allowed_origins(cls, v: str) -> List[str]:
         return v.split(",") if v else []
-    class Config:
-        env_file=".env"
-        env_file_encoding="utf-8"
-        case_sensitive=True
 
-Setting=Setting()
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+
+# Create the instance
+Setting = Setting()
